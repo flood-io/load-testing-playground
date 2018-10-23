7d1c6509d1e1308c61b19ffdb2cf1e922bea1186
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2018-10-23 13:57:38 +0200

    Adds waiting example

---
+++ element/waiting/example.ts
@@ -0,0 +1,8 @@
+import { step, Until, By } from "@flood/element";
+
+export default () => {
+  step("Wait for products", async browser => {
+    // Use CSS to find the products list, then wait until it is visible on the page
+    await browser.wait(Until.elementIsVisible(By.css(".products")));
+  });
+};
