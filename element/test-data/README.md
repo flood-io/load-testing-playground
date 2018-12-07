016e314420607e81908221f401ffc8d40342b312
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2018-12-07 15:21:22 +1100

    Adds a unique test data example

---
+++ element/test-data/README.md
@@ -0,0 +1,36 @@
+---
+stream:
+  name: Unique Test Data
+  browser_density: 40
+  duration: 18
+files:
+  - ./example.ts
+  - ./users.csv
+example:
+  highlight:
+    - 35
+    - 37
+---
+
+# Using Unique Test Data example
+
+## How to use
+
+### Using `element`
+
+Clone this repo locally, and execute [`element`][Element] to run the example on your local machine:
+
+```bash
+npx @flood/element-cli ./element/test-data/example.ts --no-headless
+```
+
+### Using [Flood](https://flood.io)
+
+Upload [example.ts](./example.ts) to an existing Stream on Flood, or [run it directly on Flood](https://app.flood.io/launch/github/flood-io/load-testing-playground/element/test-data)
+
+## The idea behind the example
+
+This example show how you can use our built in [Test Data][] support to programatically cycle through different values during each iteration of your test.
+
+[Test Data]: (https://element.flood.io/docs/1.0/api/TestData)
+[Element]: (https://github.com/flood-io/element)
---
+++ element/test-data/example.ts
@@ -0,0 +1,49 @@
+import {
+  step,
+  TestSettings,
+  Until,
+  By,
+  Browser,
+  TestData,
+  ENV
+} from "@flood/element";
+
+export const settings: TestSettings = {
+  loopCount: -1,
+  description: "Unique Test Data",
+  screenshotOnFailure: true,
+  disableCache: true,
+  clearCache: true,
+  clearCookies: true,
+  actionDelay: 1.5,
+  stepDelay: 2.5
+};
+
+/**
+ * Unique Test Data
+ * Version: 2.0
+ */
+export default () => {
+  const globalBrowserID = `${ENV.FLOOD_GRID_NODE_SEQUENCE_ID}_${
+    ENV.BROWSER_ID
+  }`;
+  interface UserData {
+    id: string;
+    username: string;
+    password: string;
+  }
+  TestData.fromCSV<UserData>("users.csv")
+    .filter((line, index, browserID) => line.id === globalBrowserID)
+    .circular();
+
+  step("Print to console", async (browser: Browser, data: UserData) => {
+    console.log(
+      "global browser ID: ",
+      globalBrowserID,
+      "| user: ",
+      `${data.username}`,
+      " | password: ",
+      `${data.password}`
+    );
+  });
+};
